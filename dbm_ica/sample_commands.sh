0d736b7162aa530ee5e3ecf3dfd37df9e63618e6
Michelle Wang <tomichellewang@gmail.com>
2022-11-21 18:18:40 -0500

    add example commands

---
+++ dbm_ica/sample_commands.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+FNAME_DOTENV=".env"
+
+# find path to containing environment variable definitions
+FPATH_CURRENT=`realpath $0`
+DPATH_CURRENT=`dirname ${FPATH_CURRENT}`
+FPATH_DOTENV="${DPATH_CURRENT}/${FNAME_DOTENV}"
+
+# load environment variable
+if [ ! -f "${FPATH_DOTENV}" ]
+then
+	echo "File not found: ${FPATH_DOTENV}"
+	exit 1
+fi
+source ${FPATH_DOTENV}
+
+# # ========================================
+# # generate T1 file list from BIDS data directory
+# # ========================================
+# COMMAND="
+# 	${FPATH_DBM_SCRIPT} bids-generate \
+# 		${DPATH_BIDS} \
+# 		${FPATH_BIDS_LIST} \
+# "
+
+# ========================================
+# submit a job to run the DBM pipeline on a single file
+# ========================================
+COMMAND="
+	${FPATH_DBM_SCRIPT} bids-run \
+		${DPATH_BIDS} \
+		${FPATH_BIDS_LIST} \
+		${DPATH_OUT_DBM} \
+		--job-type ${JOB_TYPE} \
+		--job-resource ${JOB_RESOURCE} \
+		--job-container ${FPATH_DBM_CONTAINER} \
+		-i 3 \
+"
+
+# # ========================================
+# # submit a job to run the DBM pipeline on multiple files
+# # ========================================
+# COMMAND="
+# 	${FPATH_DBM_SCRIPT} bids-run \
+# 		${DPATH_BIDS} \
+# 		${FPATH_BIDS_LIST} \
+# 		${DPATH_OUT_DBM} \
+# 		--job-type ${JOB_TYPE} \
+# 		--job-resource ${JOB_RESOURCE} \
+# 		--job-container ${FPATH_DBM_CONTAINER} \
+# 		-r 1 10 \
+# "
+
+echo ${COMMAND}
+eval "${COMMAND}"
